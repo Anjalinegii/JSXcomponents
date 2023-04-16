3672e31955f574dbb2e64924f9adc11f51e8c8e0
Anjalinegii <130498695+Anjalinegii@users.noreply.github.com>
2023-04-16 14:03:07 +0530

    Initial commit

---
+++ src/List.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+function List() {
+  return (
+    <ul>
+      <li>Your Name</li>
+
+      <li>Spirited Away</li>
+
+      <li>Weathering With You</li>
+    </ul>
+  );
+}
+export default List;
